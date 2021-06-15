c4f0fd2a1c6060664885c4309c0c89695b545a40
Fex <mailfex@mail.com>
2021-06-15 00:33:15 -0500

    agregando handler response con manejo de evento manual y  creacion de queue response

--- lambdas/websockets/handlerResponse.js
+++ lambdas/websockets/handlerResponse.js
@@ -3,12 +3,19 @@ const WebSocket = require('../common/websocketMessage');
 exports.handler = async (event) => {
     console.log('event', event);
 
+    // console.log('QueueResponse ' + event.Records.length + ' mensajes leidos');
+    // let messages = event.Records;
+    // messages.forEach(element => console.log({
+    //     "messageid": element.messageId,
+    //     "body": element.body
+    // }));
+
     await WebSocket.send({
         domainName: event.domainName,
         stage: event.stage,
         connectionID: event.connectionID,
         message: event.message,
     });
-    console.log('sent message -> '+event.message);
+    console.log('sent message -> ' + event.message);
     return Responses._200({ message: 'sent message' });
 };
